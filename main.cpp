5f1f2f294201c5ec3e526087bd175ed279022f5b
Michal Szymczak <mszymczak_94@wp.pl>
2019-03-03 12:29:13 +0100

    nowe

--- main.cpp
+++ main.cpp
@@ -4,24 +4,34 @@
 
 using namespace std;
 
+struct Ulamek
+{
+    int mianownik;
+    int licznik;
 
-struct Punkt
-{
-    int x;
-    int y;
-    int z;
-    float odleglosc(Punkt);
 };
-float Punkt::odleglosc(Punkt inny)
+Ulamek sumowanie(Ulamek *pierwszy,Ulamek *drugi)
 {
-    return sqrt(pow(x-inny.x,2)+pow(y-inny.y,2)+pow(z-inny.z,2));
+    Ulamek ulamek3;
+    ulamek3.mianownik=pierwszy->mianownik*drugi->mianownik;
+    ulamek3.licznik=pierwszy->licznik*drugi->mianownik+drugi->licznik*pierwszy->mianownik;
+    return ulamek3;
 }
 
 int main()
 {
-    Punkt a{1,2,3};
-    Punkt b{2,4,6};
-    cout<<a.odleglosc(b);
-
+    Ulamek ulamek1;
+    Ulamek ulamek2;
+    cout<<"Podaj pierwszy licznik: ";
+    cin>>ulamek1.licznik;
+    cout<<"Podaj pierwszy mianownik: ";
+    cin>>ulamek1.mianownik;
+    cout<<"Podaj drugi licznik: ";
+    cin>>ulamek2.licznik;
+    cout<<"Podaj drugi mianownik: ";
+    cin>>ulamek2.mianownik;
+    Ulamek suma=sumowanie(&ulamek1,&ulamek2);
+    cout<<suma.licznik<<endl;
+    cout<<suma.mianownik;
     return 0;
 }
