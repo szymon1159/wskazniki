6a3f4fbd3db381729a2b63e9c1f733dd48c93c60
Michal Szymczak <mszymczak_94@wp.pl>
2019-03-03 10:49:05 +0100

    fajnie123

--- main.cpp
+++ main.cpp
@@ -1,58 +1,27 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+
 using namespace std;
 
 
+struct Punkt
+{
+    int x;
+    int y;
+    int z;
+    float odleglosc(Punkt);
+};
+float Punkt::odleglosc(Punkt inny)
+{
+    return sqrt(pow(x-inny.x,2)+pow(y-inny.y,2)+pow(z-inny.z,2));
+}
 
 int main()
 {
-    string napis="KKKKKK asdasd Rownanie:3-2=4";
-    if(napis.find("Hej")!=string::npos )
-    {
-        cout<<"Dzien dobry";
-        cout<<endl;
-    }
-    if(napis.find("Pa")!=string::npos)
-    {
-        cout<<"Do widzenia";
-    }
-    // KKKKKK asdasd Rownanie:1+2=4
-    string porownanie="Rownanie:";
-    int indeks=napis.find(porownanie);
-    if (indeks != string::npos)
-    {
-        string row=napis.substr(indeks+porownanie.size(),5);
-        //1+2=7
-        cout<<row[0]<<row[1]<<row[2]<<row[3]<<row[4]<<endl;
-        if(row[1]=='+')
-        {
-            int poprawnyWynik=row[0]-48+row[2]-48;
-            if (poprawnyWynik==row[4]-48)
-            {
-                cout<<"Gratulacje!";
-            }
-            else
-            {
-                row[4] = poprawnyWynik + 48;
-                cout<<"Poprawny wynik to: "<<row;
-            }
+    Punkt a{1,2,3};
+    Punkt b{2,4,6};
+    cout<<a.odleglosc(b);
 
-        }
-        if(row[1]=='-')
-        {
-            int poprawnyWynik=row[0]-48-(row[2]-48);
-            if (poprawnyWynik==row[4]-48)
-            {
-                cout<<"Gratulacje!";
-            }
-            else
-            {
-                row[4] = poprawnyWynik + 48;
-                cout<<"Poprawny wynik to: "<<row;
-            }
-
-        }
-
-    }
     return 0;
 }
