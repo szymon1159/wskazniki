422dec51c3f6626c98ae5717aa91ed6880789e5c
Michal Szymczak <mszymczak_94@wp.pl>
2019-04-05 18:21:09 +0200

    setzadania

--- listyprzedcopy.cpp
+++ listyprzedcopy.cpp
@@ -2,72 +2,65 @@
 #include <string>
 #include <cmath>
 #include <vector>
+#include <set>
+#include <map>
 using namespace std;
 
-struct Node//element
+struct Album
 {
-    int value;
-    Node* next{nullptr};
-    ~Node()
-    {
-        if(next)
-        {
-            delete next;
+    string nazwaAlbumu;
+    string artysta;
+    int rokWydania;
+    bool operator<(const Album &other) const {
+        if (rokWydania < other.rokWydania) {
+            return true;
         }
-        cout<<"Koniec destruktora o wartosci: "<<value<<endl;
+        if (rokWydania > other.rokWydania) {
+            return false;
+        }
+        if (nazwaAlbumu < other.nazwaAlbumu) {
+            return true;
+        }
+        if (nazwaAlbumu > other.nazwaAlbumu) {
+            return false;
+        }
+        return artysta < other.artysta;
     }
 };
 
-void insert(Node* head,int new_value,int position)
-{
-    Node* beforeNew{head};
-    for(int i=0;i<position && beforeNew;++i)
-    {
-        beforeNew=beforeNew->next;
+int main() {
+    set<Album> albumy;
+    Album pierwszy;
+    pierwszy.artysta = "Linkin Park";
+    pierwszy.nazwaAlbumu = "Meteora";
+    pierwszy.rokWydania = 2003;
+    albumy.insert(pierwszy);
+    Album drugi;
+    drugi.artysta = "Linkin Park";
+    drugi.nazwaAlbumu = "Hybrid Theory";
+    drugi.rokWydania = 2003;
+    albumy.insert(drugi);
+    Album trzeci;
+    trzeci.artysta = "Pink Floyd";
+    trzeci.nazwaAlbumu = "The Piper at the Gates of Dawn";
+    trzeci.rokWydania = 1967;
+    albumy.insert(trzeci);
+    Album czwarty;
+    czwarty.artysta = "Metallica";
+    czwarty.nazwaAlbumu = "Metallica";
+    czwarty.rokWydania = 1967;
+    albumy.insert(czwarty);
+    for (const Album &element : albumy) {
+        cout << element.artysta << endl;
+        cout << element.rokWydania << endl;
+        cout << element.nazwaAlbumu << endl;
     }
-    if(not beforeNew)
-        return;
-    Node* newElement=new Node;
-    newElement->value=new_value;
-    newElement->next=beforeNew->next;
-    beforeNew->next=newElement;
-}
-void print_nodes(Node* head)
-{
-    Node* nodeToPrint{head};
-    while(nodeToPrint)
-    {
-        cout<<"Node z wartoscia: "<<nodeToPrint->value<<endl;
-        nodeToPrint=nodeToPrint->next;
-    }
-    cout<<"KONIEC LISTY"<<endl;
-}
-void remove(Node* head,int value)
-{
-    Node* obecna{head};
-    for(;obecna; obecna=obecna->next)
-    {
-        Node* nastepna=obecna->next;
-        if(nastepna->value==value);
 
-    }
 }
 
 
-int main()
-{
-    Node head;
-    head.value=5;
-    print_nodes(&head);
-    insert(&head,2,0);
-    print_nodes(&head);
-    insert(&head,1,3);
-    insert(&head,1,0);
-    print_nodes(&head);
-    insert(&head,7,2);
-    print_nodes(&head);
 
-}//
+//
 // Created by user_1 on 29.03.2019.
 //
 
